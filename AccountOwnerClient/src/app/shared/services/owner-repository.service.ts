98926c931421dfd60a06130afe9232fef67941bd
Pooya Alamirpour <pooya.alamirpour@gmail.com>
2022-08-07 19:48:44 +0430

    Owner repository is created

---
+++ AccountOwnerClient/src/app/shared/services/owner-repository.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { EnvironmentUrlService } from "./environment-url.service";
+import {HttpClient, HttpHeaders} from "@angular/common/http";
+import { Owner } from "../../_interfaces/Owner.model";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class OwnerRepositoryService {
+
+  constructor(private http: HttpClient, private envUrl: EnvironmentUrlService) { }
+
+  public getOwners = (route: string) => {
+    return this.http.get<Owner[]>(this.createCompleteRoute(route, this.envUrl.urlAddress));
+  }
+
+  public createOwner = (route: string, owner: Owner) => {
+    return this.http.post<Owner>(this.createCompleteRoute(route, this.envUrl.urlAddress), owner, this.generateHeaders());
+  }
+
+  private createCompleteRoute = (route: string, envAddress: string ) => {
+    return `${envAddress}/${route}`
+  }
+
+  private generateHeaders = () => {
+    return {
+      headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+    }
+  }
+
+}
