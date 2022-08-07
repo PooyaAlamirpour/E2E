2e23eb012c22dc60d8dd8fa135e51d62b690a675
Pooya Alamirpour <pooya.alamirpour@gmail.com>
2022-08-07 17:21:21 +0430

    Not found page is added

--- AccountOwnerClient/src/app/app.module.ts
+++ AccountOwnerClient/src/app/app.module.ts
@@ -7,12 +7,14 @@ import { HomeComponent } from './home/home.component';
 import { MenuComponent } from './menu/menu.component';
 import { CollapseModule } from "ngx-bootstrap/collapse";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { NotFoundComponent } from './error-pages/not-found/not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    MenuComponent
+    MenuComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
