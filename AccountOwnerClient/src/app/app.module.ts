0d4f5d8cf11c5e009262a8f3904a8bf9e83dfb34
Pooya Alamirpour <pooya.alamirpour@gmail.com>
2022-08-07 17:15:09 +0430

    menu is added

--- AccountOwnerClient/src/app/app.module.ts
+++ AccountOwnerClient/src/app/app.module.ts
@@ -4,15 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { MenuComponent } from './menu/menu.component';
+import { CollapseModule } from "ngx-bootstrap/collapse";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    CollapseModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
